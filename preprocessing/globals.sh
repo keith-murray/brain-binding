d76d548a643003b22e0275a41f89787692c4fbed
grace-waddell <gw0402@princeton.edu>
2026-03-04 15:14:53 -0500

    updating globals directory

--- preprocessing/globals.sh
+++ preprocessing/globals.sh
@@ -4,12 +4,12 @@ module load anacondapy/5.3.1 #you can remove this line if you are working in you
 
 # 1. Edit wherever the dicoms get transferred on the scanner
 
-#scanner_dir=/jukebox/dicom/conquest/Skyra-AWP45031/NormaL/2020 #Skyra
+scanner_dir=/jukebox/graziano/kirsten/502b #Skyra
 #scanner_dir=/jukebox/dicom/conquest/Prisma-MSTZ400D/NormaL/2020 #Prisma
-scanner_dir=/jukebox/norman/pygers/conquest #Sample project data
+#scanner_dir=/jukebox/norman/pygers/conquest #Sample project data
 
 # 2. Edit where your project directory is
-project_dir=/jukebox/YOURLAB/USERNAME/YOURSTUDY
+project_dir=/pygers_workshop/sample_study
 
 # 3. Edit where your scratch and work directories are located (note: make sure you have setup a work directory on scratch)
 scratch_dir=/jukebox/scratch/USERNAME/work/YOURSTUDY
