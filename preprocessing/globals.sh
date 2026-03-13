244eac909f6733f84d262bbe8fec4e9cb4f3dd68
grace-waddell <gw0402@princeton.edu>
2026-03-13 15:24:18 -0400

    Update globals.sh and run_fmriprep.sh

--- preprocessing/globals.sh
+++ preprocessing/globals.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-module load anacondapy/5.3.1 #you can remove this line if you are working in your own conda environment
+#module load anacondapy/5.3.1 #you can remove this line if you are working in your own conda environment
 
 # 1. Edit wherever the dicoms get transferred on the scanner
 
-scanner_dir=/jukebox/graziano/kirsten/502b #Skyra
+scanner_dir=~/NEU502B/502b #Skyra
 #scanner_dir=/jukebox/dicom/conquest/Prisma-MSTZ400D/NormaL/2020 #Prisma
 #scanner_dir=/jukebox/norman/pygers/conquest #Sample project data
 
 # 2. Edit where your project directory is
-project_dir=/pygers_workshop/sample_study
+project_dir=~/pygers_workshop/sample_study
 
 # 3. Edit where your scratch and work directories are located (note: make sure you have setup a work directory on scratch)
 scratch_dir=/jukebox/scratch/USERNAME/work/YOURSTUDY
